6f5621f4c090ded6cffe1a2c59332d044376535d
knopkloop <hruslan2007@gmail.com>
2025-10-20 12:54:49 +0300

    ready matrix input

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@ int** create(size_t rows, size_t cols);
 
 void remove(int** m, size_t rows, size_t cols);
 
-void input(int**  m, size_t rows, size_t cols);
+void input(int**  m, size_t rows, size_t cols){
+  for (size_t i = 0; i < rows; ++i){
+    for (size_t j = 0; j < cols; ++j){
+      std::cin >> m[i][j];
+    }  
+  }
+}
 
 void output(const int * const * m, size_t rows, size_t cols);
 
