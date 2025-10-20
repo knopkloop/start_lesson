f32e8baa5e6805f78d2ffe7fead4ff45105d5426
knopkloop <hruslan2007@gmail.com>
2025-10-20 13:30:52 +0300

    ready create function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 
 
 int** create(size_t rows, size_t cols) {
-  int** result = nullptr;
-  
+  int** result = new int*[rows];
+  size_t i = 0;
+  try{
+    for (; i < rows; ++i) {
+      result[i] = new int[cols];
+    }
+  }
+  catch (...) {
+    remove(result, i);
+    return nullptr;
+    throw;
+  }
 
   return result;
 }
@@ -33,13 +43,14 @@ void output(const int * const * m, size_t rows, size_t cols){
   //...
 }
 
+
 int main()
 {
 
   size_t rows = 0, cols = 0;
   std::cin >> rows >> cols;
   if (!std::cin) {
-      return 1;
+    return 1;
   }
 
   int** m = create(rows, cols);
@@ -50,6 +61,7 @@ int main()
     remove(m, rows, cols);
     return 1;
   }
+
   output(m, rows, cols);
 
   remove(m, rows);
