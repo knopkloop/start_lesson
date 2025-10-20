4e29be023d35f3e78c5bbf13d0cf5eadb6894cb5
knopkloop <hruslan2007@gmail.com>
2025-10-20 12:39:06 +0300

    function declaration

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+
+
+int** create(size_t rows, size_t cols);
+
+void remove (int** m, size_t rows, size_t cols);
+
 int main()
 {
   size_t rows = 0, cols = 0;
@@ -6,6 +12,9 @@ int main()
   if (!std::cin) {
       return 1;
   }
+
+  int** m = create (rows, cols);
   std::cout << rows << " " << cols;
   std::cout << "\n";
+  remove(m, rows, cols);
 }
